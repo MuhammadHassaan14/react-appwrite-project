1c12be3101f7c3061e1716fabae7f92944f67fda
MuhammadHassaan14 <155727306+MuhammadHassaan14@users.noreply.github.com>
2025-08-26 13:52:13 +0500

    feat:: add login, signup, input, and postcard components

---
+++ src/components/Input.jsx
@@ -0,0 +1,27 @@
+import React, {useId} from "react";
+
+const Input = React.forwardRef(function Input({
+    label,
+    type = 'text',
+    className = "",
+    ...props
+}, ref) {
+    const id = useId();
+    return (
+        <div className='w-full'>
+            {label && <label 
+            className="inline-block mb-1 pl-1"
+            htmlFor={id}>
+                {label}
+            </label>}
+            <input>
+            type={type}
+            className={`w-full px-3 py-2 border border-gray-200 rounded-lg focus:bg-gray-50 duration 200 ${className}`}
+            ref={ref}
+            {...props}
+            id={id}
+            </input>
+        </div>
+    )
+})
+export default Input;
